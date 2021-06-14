135d5723c1a789dce6a1b2f3ffefa38b8a0b87dc
C1oudwave <84195621+C1oudwave@users.noreply.github.com>
2021-06-14 11:20:16 +0300

    Update 12.py

--- 12.py
+++ 12.py
@@ -1,13 +1,14 @@
 from random import randint
 mass = [randint(-100,100) for i in range(30)]
-odd_mass = []
 max_mass = max(mass)
 index_of_max = mass.index(max_mass)
-print("Max: ", max_mass, " Index: ", index_of_max)
-def odd_list(mass, odd_mass):
+print("Max: ", max_mass, "\nIndex of max: ", index_of_max)
+def odd(mass):
+    temp_mass = []
     for i in range(len(mass)):
         if mass[i] % 2 != 0:
-            odd_mass.insert(0, mass[i])
-odd_list(mass, odd_mass)
-temp = sorted(odd_mass, reverse = True)
-print("New array:", temp)
+            temp_mass.insert(0, mass[i])
+    return temp_mass
+mass = odd(mass)
+sorted_mass = sorted(mass, reverse = True)
+print("Sorted array:", sorted_mass)
